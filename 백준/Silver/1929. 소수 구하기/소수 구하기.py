5a39c59ebdb260d3235e8d6b99eb03706cf96dc1
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2022-09-18 22:45:48 +0900

    [Silver III] Title: 소수 구하기, Time: 5420 ms, Memory: 65284 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,17 @@
+# Silver 3
+
+import sys
+
+
+M, N = map(int, sys.stdin.readline().split())
+lst = list(range(M, N+1))
+
+for i in lst:
+    if i == 1:
+        continue
+
+    for j in range(2, int(i**0.5)+1):
+        if not i % j:
+            break
+    else:
+        print(i)
