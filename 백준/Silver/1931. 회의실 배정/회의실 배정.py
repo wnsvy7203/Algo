2498abf19c9e63ea2aac52a93fdc1960229e5e35
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2022-09-22 17:48:14 +0900

    [Silver I] Title: 회의실 배정, Time: 4416 ms, Memory: 51480 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,20 @@
+# 156ms
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+time = sorted([list(map(int, input().split())) for _ in range(N)])
+time.sort(key=lambda x: x[1])
+time = deque(time)
+
+res = [time.popleft()]
+
+while time:
+    home = res[-1][1]
+    next_truck = time.popleft()
+    next_start = next_truck[0]
+
+    if home <= next_start:
+        res.append(next_truck)
+
+print(len(res))
