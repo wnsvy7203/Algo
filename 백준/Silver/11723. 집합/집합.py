9619fa897b4291c44b80c8362ec04b2aa9039b25
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2022-09-29 21:03:11 +0900

    [Silver V] Title: 집합, Time: 3980 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def string(x):
+    return str(x)
+
+
+M = int(sys.stdin.readline())
+S = []
+for _ in range(M):
+    cal = list(sys.stdin.readline().split())
+
+    if cal[0] == 'add' and int(cal[1]) not in S:
+        S.append(int(cal[1]))
+    elif cal[0] == 'remove' and int(cal[1]) in S:
+        S.remove(int(cal[1]))
+    elif cal[0] == 'check':
+        if int(cal[1]) in S:
+            print(1)
+        else:
+            print(0)
+    elif cal[0] == 'toggle':
+        if int(cal[1]) in S:
+            S.remove(int(cal[1]))
+        else:
+            S.append(int(cal[1]))
+    elif cal == ['all']:
+        S = list(range(1, 21))
+    elif cal == ['empty']:
+        S = []
