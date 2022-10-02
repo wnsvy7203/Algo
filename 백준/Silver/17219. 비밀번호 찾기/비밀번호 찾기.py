6c4c2096185870a5446e07153b0ee3494be00565
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2022-10-02 12:38:18 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 260 ms, Memory: 49788 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,11 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+txt = {}
+
+for _ in range(N):
+    site, pwd = sys.stdin.readline().split()
+    txt[site] = pwd
+
+for _ in range(M):
+    print(txt[sys.stdin.readline().rstrip()])
