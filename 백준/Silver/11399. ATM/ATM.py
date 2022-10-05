0665e2ca20ded1d94db36b5ebfb19f218168437c
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2022-10-05 13:12:32 +0900

    [Silver IV] Title: ATM, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,11 @@
+import sys
+
+N = int(sys.stdin.readline())
+P = list(map(int, sys.stdin.readline().split()))
+P.sort(reverse=True)
+
+res = 0
+for i in range(N):
+    res += sum(P[i:])
+
+print(res)
