d1d550c02f21ab3470a15246643f2c76bc973655
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2022-10-23 03:28:51 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 352 ms, Memory: 48548 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def find():
+    stk = [1]
+
+    while stk:
+        v = stk.pop()
+
+        for w in graph[v]:
+            if not visited[w]:
+                visited[w] = 1
+                parents[w] = v
+                stk.append(w)
+
+
+N = int(sys.stdin.readline())
+
+graph = [[] for _ in range(N+1)]
+
+for _ in range(N-1):
+    x, y = map(int, sys.stdin.readline().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+visited = [0 for _ in range(N+1)]
+parents = [0 for _ in range(N+1)]
+
+visited[1] = 1
+
+find()
+
+for i in range(2, N+1):
+    print(parents[i])
