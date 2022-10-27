0273cf6a3da67a29e1e1cd2d33940ccf9a3a6a7c
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2022-10-28 08:52:26 +0900

    [Silver I] Title: 카잉 달력, Time: 4272 ms, Memory: 32952 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -0,0 +1,19 @@
+import sys
+from math import gcd, lcm
+
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    M, N, X, Y = map(int, sys.stdin.readline().split())
+
+    x = X
+    y = Y
+
+    while x <= lcm(M, N):
+        if x % N == y % N:
+            print(x)
+            break
+        x += M
+    else:
+        print(-1)
