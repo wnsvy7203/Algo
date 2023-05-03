40f79d1b6211337a8efea77138c3567f2c2506d3
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-05-03 13:10:10 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+
+    cin >> A >> B;
+
+    cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A * B << "\n";
+    cout << A / B << "\n";
+    cout << A % B << "\n";
+}
