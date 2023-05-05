52e1214aa317bf158fe9b03b9dac2750c63b98a8
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-05-06 07:36:09 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+// Bronze 2. Flip Basket
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    int N;
+    
+    cin >> str >> N;
+
+    cout << str[N-1];
+}
