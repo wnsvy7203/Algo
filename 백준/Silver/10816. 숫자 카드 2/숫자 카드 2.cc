5f14965bbaf98aaa94539116890fe62a72b0df5e
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-05-31 15:34:33 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 296 ms, Memory: 84060 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -8,29 +8,31 @@ using namespace std;
 
 int N, M;
 
-
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> N;
 
     vector<int> card(N, 0);
+    vector<int> res(20000002, 0);
 
     for (int i = 0; i < N; i++)
+    {
         cin >> card[i];
-    
-    sort(card.begin(), card.end());
-    
+
+        res[card[i]+10000000]++;
+    }
+
     cin >> M;
 
     vector<int> check(M, 0);
-    vector<int> res(20000001, 0);
 
     for (int i = 0; i < M; i++)
         cin >> check[i];
-    
-    for (int i = 0; i < N; i++)
-        res[card[i]+10000000]++;
-    
+
     for (int i = 0; i < M; i++)
         cout << res[check[i]+10000000] << ' ';
 }
