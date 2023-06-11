e09d123b3d415fe53aacef94f053e22c906d19c7
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-06-11 20:49:53 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 84 ms, Memory: 6404 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,30 +1,22 @@
 // Silver 2. the Count of Linked Nodes
+// 580ms
 
 #include <iostream>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
 vector<int> graph[1001];
 int visited[1001];
 int N, M;
-stack<int> stk;
 
 void dfs(int v)
 {
-    stk.push(v);
+    visited[v] = 1;
 
-    while (!stk.empty())
-    {
-        int top = stk.top();
-        visited[top] = 1;
-        stk.pop();
-
-        for (int i = 0; i < graph[top].size(); i++)
-            if (!visited[graph[top][i]])
-                stk.push(graph[top][i]);
-    }
+    for (int i = 0; i < graph[v].size(); i++)
+        if (!visited[graph[v][i]])
+            dfs(graph[v][i]);
 }
 
 int main()
