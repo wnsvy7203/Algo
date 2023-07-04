8157bf33c7b64bcf2c750f4ba993a4646813561f
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-07-05 06:13:22 +0900

    [Gold IV] Title: 테트로미노, Time: 16 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -49,6 +49,10 @@ void dfs(int r, int c, int cnt, int tmp)
 
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> N >> M;
 
     for (int i = 0; i < N; i++)
