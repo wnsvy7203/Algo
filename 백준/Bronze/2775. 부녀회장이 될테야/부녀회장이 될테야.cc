a6a72a23902478159e866172f2125070e2dfaba0
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-07-20 10:57:45 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -1,43 +1,33 @@
-// Bronze 1. President of a Apartment Community
-
 #include <iostream>
-#include <vector>
 #include <numeric>
 
 using namespace std;
 
+int T;
+int k, n;
+int dp[15][15];
+
 int apt(int k, int n)
 {
-    vector<vector<int>> apt(15, vector<int>(14, 0));
-
     for (int i = 0; i < 15; i++)
-    {
         for (int j = 0; j < 14; j++)
-        {
-            if (i == 0)
-            {
-                apt[i][j] = j + 1;
-            }
+            if (!i)
+                dp[i][j] = j + 1;
             else
-            {
-                apt[i][j] = accumulate(apt[i-1].begin(), apt[i-1].begin()+j+1, 0);
-            }
-        }
-    }
+                dp[i][j] = accumulate(dp[i-1], dp[i-1]+j+1, 0);
 
-    return apt[k][n-1];
+    return dp[k][n-1];
 }
 
 int main()
 {
-    int T;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
     cin >> T;
 
-    for (int t = 0; t < T; t++)
+    while (T--)
     {
-        int k, n;
-
         cin >> k >> n;
 
         cout << apt(k, n) << '\n';
