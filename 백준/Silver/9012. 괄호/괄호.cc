7b0b40d2bce8dd184962400960410b8f1c74fbb7
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-07-20 16:55:25 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,15 +1,16 @@
-// Silver 4. Parenthesis
-
 #include <iostream>
 #include <stack>
 #include <string>
 
 using namespace std;
 
+int T;
+
 int main()
 {
-    int T;
-
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
     cin >> T;
 
     while (T--)
@@ -23,11 +24,9 @@ int main()
         cin >> p;
 
         for (int i = 0; i < p.length(); i++)
-        {
             if (p[i] == '(')
                 stk.push(1);
             else if (p[i] == ')')
-            {
                 if (stk.empty())
                 {
                     check = 0;
@@ -35,19 +34,13 @@ int main()
                 }
                 else
                     stk.pop();
-            }
-        }
 
         if (check)
-        {
             if (stk.empty())
                 cout << "YES" << '\n';
             else
                 cout << "NO" << '\n';
-        }
         else
-        {
             cout << "NO" << '\n';
-        }
     }
 }
