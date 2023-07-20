a1e3634a63e9ffcfee7280444fdbbdca5415db21
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-07-20 11:36:06 +0900

    [Silver V] Title: 단어 정렬, Time: 44 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,50 +1,41 @@
-// Silver 5. Sorting Words
+// 92ms
 
 #include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
+string arr[20001];
+
 bool compare(string a, string b)
 {
     if (a.length() != b.length())
-    {
         return a.length() < b.length();
-    }
     else
-    {
         return a < b;
-    }
 }
 
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
     int N;
 
     cin >> N;
 
-    vector<string> str;
+    string word;
+
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    sort(arr, arr+N, compare);
 
     for (int i = 0; i < N; i++)
     {
-        string word;
-
-        cin >> word;
-
-        str.push_back(word);
-    }
-
-    sort(str.begin(), str.end(), compare);
-
-    for (int i = 0; i < str.size(); i++)
-    {
-        if (i != 0 && str[i] == str[i-1])
-        {
+        if (i && arr[i] == arr[i-1])
             continue;
-        }
 
-        cout << str[i] << '\n';
+        cout << arr[i] << '\n';
     }
 }
