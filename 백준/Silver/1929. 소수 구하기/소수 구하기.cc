fd0fbfab69a038d8be8b3b740bba0d504008b9dd
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-07-20 17:44:05 +0900

    [Silver III] Title: 소수 구하기, Time: 16 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,58 +1,32 @@
 // Silver 3. Finding Prime Numbers
+// 20ms
 
 #include <iostream>
 #include <vector>
 #include <cmath>
+#define MAX 1000001
 
 using namespace std;
 
 int M, N;
+bool prime[MAX];
 
-// 436ms
-// int main()
-// {
-//     cin >> M >> N;
-
-//     for (int i = M; i <= N; i++)
-//     {
-//         bool flag = 1;
-
-//         if (i == 1) flag = 0;
-//         else if (i == 2) flag = 1;
-//         else
-//             for (int j = 2; j < sqrt(i)+1; j++)
-//                 if (!(i % j))
-//                 {
-//                     flag = 0;
-//                     break;
-//                 }
-
-//         if (flag)   cout << i << '\n';
-//     }
-// }
+void eratos()
+{
+    for (int i = 2; i < MAX; i++)
+        if (prime[i])
+            for (int j = 2*i; j < MAX; j += i)
+                prime[j] = 0;
+}
 
 int main()
 {
-    vector<bool> prime(1000001, true);
-
-    prime[0] = 0;
-    prime[1] = 0;
-
-    int cnt = 2;
-
-    while (cnt <= 1000000)
-    {
-        if (prime[cnt])
-            for (int i = 2 * cnt; i < 1000001; i += cnt)
-                prime[i] = 0;
-        
-        cnt++;
-    }
+    fill(prime+2, prime+MAX, 1);
+    eratos();
 
     cin >> M >> N;
 
     for (int i = M; i <= N; i++)
-    {
-        if (prime[i])   cout << i << '\n';
-    }
+        if (prime[i])
+            cout << i << '\n';
 }
