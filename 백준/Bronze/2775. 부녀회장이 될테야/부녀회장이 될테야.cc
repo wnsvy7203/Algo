a6d54655b79670b412e1d15be991a0acb0a0e7b5
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-07-20 11:00:10 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -5,9 +5,9 @@ using namespace std;
 
 int T;
 int k, n;
-int dp[15][15];
+int dp[15][14];
 
-int apt(int k, int n)
+void apt()
 {
     for (int i = 0; i < 15; i++)
         for (int j = 0; j < 14; j++)
@@ -15,8 +15,6 @@ int apt(int k, int n)
                 dp[i][j] = j + 1;
             else
                 dp[i][j] = accumulate(dp[i-1], dp[i-1]+j+1, 0);
-
-    return dp[k][n-1];
 }
 
 int main()
@@ -24,12 +22,14 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
+		apt();
+
     cin >> T;
 
     while (T--)
     {
         cin >> k >> n;
 
-        cout << apt(k, n) << '\n';
+        cout << dp[k][n-1] << '\n';
     }
 }
