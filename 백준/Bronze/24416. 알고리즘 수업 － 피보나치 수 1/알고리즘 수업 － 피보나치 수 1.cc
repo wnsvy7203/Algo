124a7ef9536a734f210382c8a08e8016d67e8e2a
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-08-08 02:30:12 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.cc
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.cc
@@ -5,7 +5,7 @@
 using namespace std;
 
 int n;
-int dp[42];
+int dp[41];
 
 int main()
 {
@@ -17,7 +17,7 @@ int main()
     dp[1] = 1;
     dp[2] = 1;
 
-    for (int i = 3; i <= n+1; i++)
+    for (int i = 3; i <= n; i++)
         dp[i] = dp[i-1] + dp[i-2];
 
     cout << dp[n] << ' ' << n-2;
