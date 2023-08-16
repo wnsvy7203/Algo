cbf0484f4f07836c15d849e65d101bf194cf575f
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-08-17 07:38:26 +0900

    [Silver II] Title: 친구 팰린드롬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15270. 친구 팰린드롬/친구 팰린드롬.cc
@@ -0,0 +1,58 @@
+// 8ms
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> graph[21];
+bool visited[21];
+
+int dfs(int n, int cnt)
+{
+    if (n > N)
+        return cnt;
+    
+    if (visited[n])
+        return dfs(n+1, cnt);
+
+    int res = 0;
+    for (int i : graph[n])
+        if (!visited[i])
+        {
+            visited[i] = 1;
+            res = max(res, dfs(n+1, cnt+1));
+            visited[i] = 0;
+        }
+    
+    return max(res, dfs(n+1, cnt));
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+    
+    int u, v;
+    while (M--)
+    {
+        cin >> u >> v;
+
+        if (u > v)
+            swap(u, v);
+
+        graph[u].push_back(v);
+    }
+
+    int res = dfs(1, 0);
+
+    res *= 2;
+    
+    if (res < N)
+        res++;
+    
+    cout << res;
+}
