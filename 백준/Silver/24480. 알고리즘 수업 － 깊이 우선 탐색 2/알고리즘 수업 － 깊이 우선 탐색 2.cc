fbbed71a3ef3ce7993dc66bc20996b6b1a139e43
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-08-19 20:46:32 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 136 ms, Memory: 10932 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,50 @@
+// 8ms
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define MAX 100001
+
+using namespace std;
+
+int N, M, R;
+vector<int> E[MAX];
+int visited[MAX];
+
+int cnt = 0;
+
+void dfs(int r)
+{
+    cnt++;
+    visited[r] = cnt;
+
+    for (int x : E[r])
+        if (!visited[x])
+            dfs(x);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M >> R;
+    
+    int u, v;
+    while (M--)
+    {
+        cin >> u >> v;
+
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+
+    for (int i = 1; i <= N; i++)
+        sort(E[i].begin(), E[i].end(), greater<>());
+
+    dfs(R);
+    
+    for (int i = 1; i <= N; i++)
+        cout << visited[i] << '\n';
+}
