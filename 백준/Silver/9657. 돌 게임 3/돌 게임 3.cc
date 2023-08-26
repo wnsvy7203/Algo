ab7b6daacfcaded42d910c14f884865f4c4185fd
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-08-26 23:58:21 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -1,7 +1,6 @@
 //
 
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
@@ -10,19 +9,18 @@ int dp[1001];
 
 int main()
 {
-    cin >> N;
-
     dp[1] = 1;
     dp[2] = 0;
     dp[3] = 1;
     dp[4] = 1;
 
-    for (int i = 5; i <= N; i++)
-        if (min({dp[i-1], dp[i-3], dp[i-4]}))
+    for (int i = 5; i < 1001; i++)
+        if (dp[i-1] && dp[i-3] && dp[i-4])
             dp[i] = 0;
         else
             dp[i] = 1;
     
+    cin >> N;
     if (dp[N])
         cout << "SK";
     else
