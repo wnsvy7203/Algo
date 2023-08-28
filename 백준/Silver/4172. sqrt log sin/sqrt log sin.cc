e7bb4f00130bf692b789f7ecdd60c44dbbb9c1e2
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-08-28 21:12:01 +0900

    [Silver III] Title: sqrt log sin, Time: 496 ms, Memory: 6096 KB -BaekjoonHub

---
+++ 백준/Silver/4172. sqrt log sin/sqrt log sin.cc
@@ -0,0 +1,33 @@
+//
+
+#include <iostream>
+#include <cmath>
+
+#define MAX 1000001
+#define DIV 1000000
+
+using namespace std;
+
+int i;
+int ans[MAX];
+
+int main()
+{
+    ans[0] = 1;
+    for (int x = 1; x < MAX; x++)
+    {
+        int a = (int)floor(x-sqrt(x));
+        int b = (int)log(x);
+        int c = (int)(x*sin(x)*sin(x));
+
+        ans[x] = (ans[a] + ans[b] + ans[c]) % DIV;
+    }
+
+    while (cin >> i)
+    {
+        if (i == -1)
+            return 0;
+        
+        cout << ans[i] << '\n';
+    }
+}
