0629cb04582c1b5b0b9d354544b3ab1256b12d71
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-09-02 23:47:08 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -0,0 +1,29 @@
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+int L[21], J[21];
+int dp[101];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+        cin >> L[i];
+    
+    for (int i = 1; i <= N; i++)
+        cin >> J[i];
+    
+    for (int i = 1; i <= N; i++)
+        for (int j = 100; j >= L[i]; j--)
+            dp[j] = max(dp[j], dp[j-L[i]] + J[i]);
+    
+    cout << dp[99];
+}
