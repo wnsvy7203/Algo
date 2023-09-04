39b38e813c27aa4b54ab5c43c26c7d371c6167c7
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-09-04 19:06:06 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.cc
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -1,5 +1,3 @@
-//
-
 #include <iostream>
 #include <algorithm>
 
@@ -22,7 +20,7 @@ int main()
         dp[i] = 1;
     
     for (int i = 1; i < n; i++)
-        for (int j = i-1; j >= 0; j--)
+        for (int j = 0; j < i; j++)
             if (big[i] > big[j])
                 dp[i] = max(dp[i], dp[j] + 1);
 
