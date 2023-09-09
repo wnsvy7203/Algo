7df1c73ae4a6a855a973efe9c1d46f6e748c2d84
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-09-09 18:26:20 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -9,14 +9,14 @@ int dp[1001];
 
 int main()
 {
-    cin >> N;
-    
     dp[1] = 1;
     dp[3] = 1;
 
     for (int i = 5; i <= 1000; i++)
         if (!dp[i-1] && !dp[i-3] && !dp[i-4])
             dp[i] = 1;
+    
+    cin >> N;
     
     if (dp[N])
         cout << "CY";
