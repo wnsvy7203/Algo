999bf64af94753dcb2c3b9b4b8cc2367454d57af
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-09-20 19:55:54 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string cipher, int code) {
+    string answer = "";
+    for (int i = 0; i < cipher.size(); i++)
+        if (!((i+1) % code))
+            answer += cipher[i];
+    return answer;
+}
