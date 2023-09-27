331c6d69abb64adc97ae2c17ee1472eef6a9143e
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-09-27 20:34:23 +0900

    [Silver II] Title: 회의실 배정 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.cc
@@ -0,0 +1,31 @@
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+int time[26][3];
+long long int dp[26][2];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> time[i][0] >> time[i][1] >> time[i][2];
+
+    dp[0][1] = time[0][2];
+    dp[1][1] = time[1][2];
+    dp[1][0] = time[0][2];
+
+    for (int i = 2; i < N; i++)
+    {
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
+        dp[i][1] = dp[i-1][0] + time[i][2];
+    }
+
+    cout << max(dp[N-1][0], dp[N-1][1]);
+}
