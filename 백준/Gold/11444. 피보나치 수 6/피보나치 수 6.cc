54b025cf83054d2c75c406695c17ab2da7da97c2
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-11-16 00:03:36 +0900

    [Gold II] Title: 피보나치 수 6, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
@@ -10,13 +10,18 @@ map<unsigned long long, unsigned long long> m;
 
 unsigned long long fibo(unsigned long long x)
 {
+    if (x == 0 || x == 1)
+        return x;
+    else if (x == 2)
+        return 1;
+
     if (m[x])
         return m[x];
 
     if (!(x % 2))
-        m[x] = ((fibo(x/2) % DIV) * ((fibo(x/2+1) % DIV + fibo(x/2-1) % DIV) % DIV)) % DIV;
+        m[x] = fibo(x/2) * ((fibo(x/2+1) + fibo(x/2-1)) % DIV) % DIV;
     else
-        m[x] = ((((fibo((x+1)/2) % DIV) * (fibo((x+1)/2) % DIV)) % DIV) + (((fibo((x-1)/2) % DIV) * (fibo((x-1)/2) % DIV)) % DIV)) % DIV;
+        m[x] = ((fibo((x+1)/2) * fibo((x+1)/2) % DIV) + (fibo((x-1)/2) * fibo((x-1)/2) % DIV)) % DIV;
     
     return m[x];
 }
@@ -25,9 +30,6 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    m[0] = 0;
-    m[1] = m[2] = 1;
 
     cin >> n;
     cout << fibo(n);
