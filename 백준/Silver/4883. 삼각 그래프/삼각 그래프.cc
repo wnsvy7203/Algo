9bd5e34dee4164e1114cd88113bcceb7d138d577
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-11-18 01:28:18 +0900

    [Silver I] Title: 삼각 그래프, Time: 24 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.cc
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.cc
@@ -1,42 +1,38 @@
 #include <iostream>
 #include <algorithm>
 
-#define MAX 100001
-
 using namespace std;
 
-int graph[MAX][3], dp[MAX][3];
+int T, N;
+int board[100001][3], dp[100001][3];
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int ans = 0;
     while (1)
     {
-        ans++;
-        int N;
-        cin >> N;
+        T++;
 
-        if (!N)
+        cin >> N;
+        if (N == 0)
             break;
         
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < 3; j++)
-                cin >> graph[i][j];
+        for (int i = 1; i <= N; i++)
+            cin >> board[i][0] >> board[i][1] >> board[i][2];
         
-        dp[0][0] = 987654321;
-        dp[0][1] = graph[0][1];
-        dp[0][2] = graph[0][1] + graph[0][2];
+        dp[1][0] = 1000;
+        dp[1][1] = board[1][1];
+        dp[1][2] = board[1][1] + board[1][2];
 
-        for (int i = 1; i < N; i++)
+        for (int i = 2; i <= N; i++)
         {
-            dp[i][0] = min(dp[i-1][0], dp[i-1][1]) + graph[i][0];
-            dp[i][1] = min({dp[i-1][0], dp[i-1][1], dp[i-1][2], dp[i][0]}) + graph[i][1];
-            dp[i][2] = min({dp[i-1][1], dp[i-1][2], dp[i][1]}) + graph[i][2];
+            dp[i][0] = board[i][0] + min(dp[i-1][0], dp[i-1][1]);
+            dp[i][1] = board[i][1] + min({dp[i-1][0], dp[i-1][1], dp[i-1][2], dp[i][0]});
+            dp[i][2] = board[i][2] + min({dp[i-1][1], dp[i-1][2], dp[i][1]});
         }
 
-        cout << ans << ". " << dp[N-1][1] << '\n';
+        cout << T << ". " << dp[N][1] << '\n';
     }
 }
