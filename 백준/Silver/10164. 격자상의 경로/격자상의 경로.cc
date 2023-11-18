e2bc74db536f8e2dcede0e2a21013bc0062b6a56
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-11-19 00:21:31 +0900

    [Silver I] Title: 격자상의 경로, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int N, M, K;
-int board[16][16], dp[16][16];
+int dp[16][16];
 
 int R, C;
 
@@ -13,44 +13,36 @@ int main()
     cin.tie(NULL);
 
     cin >> N >> M >> K;
-    int num = 1;    bool flag = 0;
+    int num = 1;
     for (int i = 1; i <= N; i++)
         for (int j = 1; j <= M; j++)
         {
-            board[i][j] = num++;
-            if (board[i][j] == K)
+            if (num++ == K)
             {
-                flag = 1;
                 R = i;
                 C = j;
             }
         }
     
     dp[1][1] = 1;
-    for (int i = 2; i <= M; i++)
-        dp[1][i] = 1;
-    for (int i = 2; i <= N; i++)
-        dp[i][1] = 1;
-    
-    if (flag)
+    if (K)
     {
-        for (int i = 2; i <= R; i++)
-            for (int j = 2; j <= C; j++)
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for (int i = 1; i <= R; i++)
+            for (int j = 1; j <= C; j++)
+                dp[i][j] = dp[i][j] + dp[i-1][j] + dp[i][j-1];
         
-        for (int i = C+1; i <= M; i++)
-            dp[R][i] = dp[R][C];
-        for (int i = R+1; i <= N; i++)
-            dp[i][C] = dp[R][C];
-        
-        for (int i = R+1; i <= N; i++)
-            for (int j = C+1; j <= M; j++)
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for (int i = R; i <= N; i++)
+            for (int j = C; j <= M; j++)
+            {
+                if (i == R && j == C)
+                    continue;
+                dp[i][j] = dp[i][j] + dp[i-1][j] + dp[i][j-1];
+            }
     }
     else
-        for (int i = 2; i <= M; i++)
-            for (int j = 2; j <= M; j++)
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for (int i = 1; i <= N; i++)
+            for (int j = 1; j <= M; j++)
+                dp[i][j] = dp[i][j] + dp[i-1][j] + dp[i][j-1];
     
     cout << dp[N][M];
 }
