6e698e45d9dda1a5a17590382b68073217f5fb30
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-11-24 00:43:47 +0900

    [Gold III] Title: 아기 상어, Time: 4 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -1,3 +1,5 @@
+// 8ms
+
 #include <iostream>
 #include <queue>
 #include <tuple>
@@ -11,17 +13,17 @@ int R, C, ans, cnt;
 int dr[4] = {-1, 1, 0, 0};
 int dc[4] = {0, 0, -1, 1};
 
-bool compare(tuple<int, int, int> a, tuple<int, int, int> b)
-{
-    if (get<2>(a) == get<2>(b))
-    {
-        if (get<0>(a) == get<0>(b))
-            return get<1>(a) < get<1>(b);
-        return get<0>(a) < get<0>(b);
-    }
+// bool compare(tuple<int, int, int> a, tuple<int, int, int> b)
+// {
+//     if (get<2>(a) == get<2>(b))
+//     {
+//         if (get<0>(a) == get<0>(b))
+//             return get<1>(a) < get<1>(b);
+//         return get<0>(a) < get<0>(b);
+//     }
 
-    return get<2>(a) < get<2>(b);
-}
+//     return get<2>(a) < get<2>(b);
+// }
 
 queue<pair<int, int>> que;
 tuple<int, int, int> bfs(int y, int x, int weight)
@@ -46,18 +48,23 @@ tuple<int, int, int> bfs(int y, int x, int weight)
             if (nr < 0 || nr >= N || nc < 0 || nc >= N)
                 continue;
             
+                // 아기 상어의 현재 크기와 같은 먹이는 지나갈 수는 있지만 먹을 수는 없다.
             if (!dist[nr][nc] && board[nr][nc] <= weight)
             {
                 que.push({nr, nc});
                 dist[nr][nc] = dist[r][c] + 1;
 
+                // 아기 상어의 현재 크기보다 작은 것만 먹을 수 있다.
+                // 먹을 수 있는 먹이의 위치 (nr, nc)와 현재 아기 상어의 위치에서부터의 거리를 tuple로 만들어 v에 담는다.
+                // 거리를 1에서부터 시작했기 때문에, 거리는 -1 해서 넣어준다.
                 if (board[nr][nc] && board[nr][nc] < weight)
-                    v.push_back({nr, nc, dist[nr][nc]-1});
+                    v.push_back({dist[nr][nc]-1, nr, nc});
             }
         }
     }
 
-    sort(v.begin(), v.end(), compare);
+    // v를 정렬하는데, 정렬 기준은 거리
+    sort(v.begin(), v.end());
     if (v.empty())
         return {0, 0, 0};
 
@@ -76,6 +83,7 @@ int main()
             cin >> board[i][j];
             if (board[i][j] == 9)
             {
+                // 아기 상어의 위치를 기억해둔다.
                 R = i;
                 C = j;
             }
@@ -84,11 +92,12 @@ int main()
     int shark = 2;
     while (1)
     {
+        // 아기 상어의 현재 위치에서 bfs
         tuple<int, int, int> res = bfs(R, C, shark);
 
-        int NR = get<0>(res);
-        int NC = get<1>(res);
-        int far = get<2>(res);
+        int far = get<0>(res);
+        int NR = get<1>(res);
+        int NC = get<2>(res);
 
         if (!NR && !NC && !far)
             break;
