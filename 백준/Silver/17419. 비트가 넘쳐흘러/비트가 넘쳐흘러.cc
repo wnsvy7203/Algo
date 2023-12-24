6d5fbf3c07f27b4d7acd4ec43866fadb49efa021
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2023-12-24 19:41:52 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 8 ms, Memory: 3680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.cc
+++ 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.cc
@@ -6,62 +6,16 @@ using namespace std;
 int N;
 string K;
 
-string cal(string a)
-{
-    reverse(a.begin(), a.end());
-    int cnt = 0;
-    for (char c : a)
-    {
-        if (c == '0')
-            cnt++;
-        else
-        {
-            cnt++;
-            break;
-        }
-    }
-
-    string b = "";
-    for (int i = 0; i < cnt; i++)
-        b += '1';
-    
-    int len = a.length();
-    string c = "";
-    for (int i = 0; i < cnt; i++)
-        c += '0';
-    for (int i = cnt; i < len; i++)
-        c += a[i];
-    
-    reverse(c.begin(), c.end());
-    return c;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> N >> K;
-    int ans = 1;
-    while (1)
-    {
-        K = cal(K);
-
-        int len = K.length();
-        bool flag = 1;
-        for (int i = 0; i < len; i++)
-            if (K[i] == '1')
-            {
-                flag = 0;
-                break;
-            }
-        
-        if (flag)
-        {
-            cout << ans;
-            break;
-        }
-        else
+    int ans = 0;
+    for (int i = 0; i < N; i++)
+        if (K[i] == '1')
             ans++;
-    }
+    
+    cout << ans;
 }
