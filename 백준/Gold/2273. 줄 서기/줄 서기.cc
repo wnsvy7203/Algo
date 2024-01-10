36f48ee655c6fc6d2a5f074e3a16a64dd8ae8c46
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2024-01-10 22:33:59 +0900

    [Gold III] Title: 줄 서기, Time: 32 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Gold/2273. 줄 서기/줄 서기.cc
+++ 백준/Gold/2273. 줄 서기/줄 서기.cc
@@ -1,3 +1,5 @@
+// 32ms
+
 #include <iostream>
 
 using namespace std;
@@ -5,47 +7,34 @@ using namespace std;
 int N, M;
 bool graph[257][257];
 
-void floyd()
+void input()
 {
-
-}
-
-void find_answer()
-{
-
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     cin >> N >> M;
     while (M--)
     {
         int A, B;
         cin >> A >> B;
         graph[A][B] = 1;
-
-        if (graph[A][B] && graph[B][A])
-        {
-            cout << -1;
-            return 0;
-        }
     }
+}
 
+void floyd()
+{
     for (int k = 1; k <= N; k++)
         for (int i = 1; i <= N; i++)
             for (int j = 1; j <= N; j++)
                 if (graph[i][k] && graph[k][j])
                     graph[i][j] = 1;
-    
+}
+
+void find_answer()
+{
     for (int i = 1; i <= N; i++)
         for (int j = 1; j <= N; j++)
             if (graph[i][j] && graph[j][i])
             {
                 cout << -1;
-                return 0;
+                return;
             }
 
     for (int i = 1; i <= N; i++)
@@ -63,4 +52,14 @@ int main()
 
         cout << tall << ' ' << small << '\n';
     }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    input();
+    floyd();
+    find_answer();
 }
