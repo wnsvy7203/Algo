39587ee72109a3308c834e3b16831283fa8fcee5
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2024-01-29 02:40:01 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,4 +1,4 @@
-// Gold 4. Floyd
+// 28ms
 
 #include <iostream>
 #define INF 10000000
@@ -8,23 +8,8 @@ using namespace std;
 int n, m;
 int graph[101][101];
 
-void floyd()
+void init()
 {
-    for (int k = 1; k <= n; k++)
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                if (i == j || j == k || k == i)
-                    continue;
-                else if (graph[i][j] > graph[i][k] + graph[k][j])
-                    graph[i][j] = graph[i][k] + graph[k][j];
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
     cin >> n >> m;
 
     for (int i = 1; i <= n; i++)
@@ -36,26 +21,42 @@ int main()
                 graph[i][j] = INF;
         }
 
-    int a, b, c;
-
-    for (int i = 0; i < m; i++)
+    while (m--)
     {
+        int a, b, c;
         cin >> a >> b >> c;
-
         graph[a][b] = min(graph[a][b], c);
     }
+}
 
-    floyd();
+void floyd()
+{
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+}
 
+void print()
+{
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
-        {
             if (graph[i][j] == INF)
                 cout << 0 << ' ';
             else
                cout << graph[i][j] << ' ';
-        }
+
         cout << '\n';
     }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    init();
+    floyd();
+    print();
 }
