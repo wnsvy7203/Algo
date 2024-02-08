e1a5eda3e867cd36e4734b9c60d8b692030a6045
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2024-02-09 08:31:59 +0900

    [Gold IV] Title: CTP 왕국은 한솔 왕국을 이길 수 있을까?, Time: 44 ms, Memory: 2940 KB -BaekjoonHub

---
+++ 백준/Gold/15789. CTP 왕국은 한솔 왕국을 이길 수 있을까？/CTP 왕국은 한솔 왕국을 이길 수 있을까？.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int N, M, C, H, K;
+int par[100001], ally[100001];
+
+int find(int x)
+{
+    if (par[x] == x)
+        return x;
+    
+    return par[x] = find(par[x]);
+}
+
+void union_sets(int x, int y)
+{
+    x = find(x);
+    y = find(y);
+
+    if (x > y)
+        swap(x, y);
+
+    par[y] = x;
+    ally[x] += ally[y];
+}
+
+void init()
+{
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++)
+    {
+        par[i] = i;
+        ally[i] = 1;
+    }
+}
+
+priority_queue<int> pq;
+void settings()
+{
+    while (M--)
+    {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (find(X) == find(Y))
+            continue;
+
+        union_sets(X, Y);
+    }
+
+    cin >> C >> H >> K;
+    C = find(C);
+    H = find(H);
+
+    for (int i = 1; i <= N; i++)
+        if (par[i] == i && i != C && i != H)
+            pq.push(ally[i]);
+
+    while (K--)
+    {
+        int t = pq.top();
+        pq.pop();
+
+        ally[C] += t;
+    }
+}
+
+void find_answer()
+{
+    cout << ally[C];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    init();
+    settings();
+    find_answer();
+}
