c6490b795df1c42447bdb2783504d5189891beb1
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2024-02-13 22:41:23 +0900

    [Gold IV] Title: Σ, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/13172. Σ/Σ.cc
+++ 백준/Gold/13172. Σ/Σ.cc
@@ -6,25 +6,21 @@ using namespace std;
 
 long long M, N, S, g, ans;
 
-long long sigma(long long a, long long b)
+long long Fermat(long long a, long long b)
 {
     if (b == 1)
         return a;
-    else if (b%2)
-        return a * sigma(a, b-1) % MOD;
+    else if (b % 2)
+        return a * Fermat(a, b-1) % MOD;
     
-    long long x = sigma(a, b/2);
+    long long x = Fermat(a, b/2);
     return x * x % MOD;
 }
 
 long long gcd(long long a, long long b)
 {
     if (a < b)
-    {
-        long long tmp = a;
-        a = b;
-        b = tmp;
-    }
+        swap(a, b);
     
     while (b)
     {
@@ -50,7 +46,7 @@ int main()
         N /= g;
         S /= g;
 
-        ans += (S * sigma(N, MOD-2)) % MOD;
+        ans += (S * Fermat(N, MOD-2)) % MOD;
         ans %= MOD;
     }
 
