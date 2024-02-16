4891ed36fccbcc95e51d5857f287b744f69a414e
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2024-02-17 01:53:37 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 5.55 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.cpp
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.cpp
@@ -8,14 +8,14 @@ int answer;
 vector<double> degree(int s)
 {
     int h = s / 3600;
-    s -= h * 3600;
+    s -= h;
     
     int m = s / 60;
-    s -= m * 60;
+    s -= m;
     
-    double d_hour = (h % 12) * 30.0 + m * 0.5 + s / 120.0;
-    double d_minute = m * 6.0 + s * 0.1;
-    double d_second = s * 6.0;
+    double d_hour = (h % 12) * 30 + m * 0.5 + s / 120;
+    double d_minute = m * 6 + s * 0.1;
+    double d_second = s * 6;
     
     return {d_hour, d_minute, d_second};
 }
@@ -23,28 +23,30 @@ vector<double> degree(int s)
 int solution(int h1, int m1, int s1, int h2, int m2, int s2)
 {
     int start = h1*3600 + m1*60 + s1;
-    int end = h2*3600 + m2*60 + s2;
+    int end = h2*3600 * m2*60 + s2;
     
     vector<double> v1 = degree(start);
     if (v1[0] == v1[1] || v1[1] == v1[2] || v1[2] == v1[0])
         answer++;
     
     for (int now = start; now < end; now++)
-    {
+    {        
         vector<double> cur = degree(now);
         vector<double> next = degree(now+1);
         
+        int cnt = 0;
+        
         // 1. 초침과 시침이 겹친 경우
         if (cur[0] > cur[2] && next[0] <= next[2] && next[0] != next[1])
-            answer++;
+            cnt++;
         else if (cur[0] > 354 && cur[2] == 354 && next[0] != next[1])
-            answer++;    
+            cnt++;    
         
         // 2. 초침과 분침이 겹친 경우
         if (cur[1] > cur[2] && next[1] <= next[2] && next[0] != next[1])
-            answer++;
+            cnt++;
         else if (cur[1] > 354 && cur[2] == 354 && next[0] != next[1])
-            answer++;
+            cnt++;
         
         // 3. 분침과 시침이 겹친 경우
         if (next[0] == next[1])
