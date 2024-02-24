6f0aa6485512067db7d98c1a20cc757418dce479
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2024-02-25 00:36:06 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 1.79 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,6 +10,9 @@ int solution(vector<int> queue1, vector<int> queue2) {
     int n = queue1.size();
     long long sum1 = 0, sum2 = 0;
     queue<int> q1, q2;
+    
+    int MAX = 0;
+    
     for (int i = 0; i < n; i++)
     {
         sum1 += queue1[i];
@@ -16,14 +20,21 @@ int solution(vector<int> queue1, vector<int> queue2) {
         
         sum2 += queue2[i];
         q2.push(queue2[i]);
+        
+        MAX = max({MAX, queue1[i], queue2[i]});
     }
+    
+    if ((sum1 + sum2) % 2)
+        return -1;
+    
+    if (MAX * 2 > sum1 + sum2)
+        return -1;
     
     while (sum1 != sum2)
     {
-        if (answer > n * 2 + 1)
+        if (answer > n * 3)
             return -1;
-        
-        answer++;
+
         if (sum1 > sum2)
         {
             int num = q1.front();
@@ -40,6 +51,8 @@ int solution(vector<int> queue1, vector<int> queue2) {
             sum2 -= num;
             q2.pop();
         }
+        
+        answer++;
     }
     
     return answer;
