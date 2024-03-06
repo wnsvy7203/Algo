463e0657e359f63fe8b24fecd29d57d0d4f6a041
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2024-03-07 00:11:14 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 44 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define MAX 2147483647
+
+using namespace std;
+
+int N;
+int arr[501][2], dp[501][501];
+
+void init()
+{
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+        cin >> arr[i][0] >> arr[i][1];
+}
+
+void dynamic_programming()
+{
+    for (int i = 1; i < N; i++)
+    {
+        for (int j = 1; i+j <= N; j++)
+        {
+            dp[j][i+j] = MAX;
+            for (int k = j; k <= i+j; k++)
+                dp[j][i+j] = min(dp[j][i+j], dp[j][k] + dp[k+1][i+j] + arr[j][0] * arr[k][1] * arr[i+j][1]);
+        }
+    }
+
+    cout << dp[1][N];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    init();
+    dynamic_programming();
+}
