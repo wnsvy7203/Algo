66b985a36be7a73f1a2c5ada70887acadb6b2a78
wnsvy7203 <109322437+wnsvy7203@users.noreply.github.com>
2024-03-12 22:19:00 +0900

    [level 2] Title: [1차] 캐시, Time: 0.24 ms, Memory: 3.85 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -1,37 +1,35 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <deque>
 
 using namespace std;
 
 int solution(int cacheSize, vector<string> cities) {
     int answer = 0;
+
+    // 만약 캐시 크기가 0이라면 deque에 넣을 필요도 없이 5초씩 소요된다.
     if (!cacheSize)
         return cities.size() * 5;
     
+    // 대소문자의 구분이 없다고 했으므로 모든 cities의 원소들을 대문자로 바꿔준다.
     for (int i = 0; i < cities.size(); i++)
         for (int j = 0; j < cities[i].size(); j++)
             if (cities[i][j] >= 97 && cities[i][j] <= 122)
                 cities[i][j] -= 32;
     
+    // deque 선언
     deque<string> deq;
+    // cities의 원소들을 순회하면서 확인한다.
     for (string str : cities)
     {
-        bool flag = 0;
-        int idx = -1;
+        // 만약 
+        auto it = find(deq.begin(), deq.end(), str);
         
-        for (int j = 0; j < deq.size(); j++)
+        if (it != deq.end())
         {
-            if (deq[j] == str)
-            {
-                flag = 1;
-                idx = j;
-                break;
-            }
-        }
-        
-        if (flag)
-        {
+            int idx = it - deq.begin();
+
             answer++;
             string hit = deq[idx];
             deq.erase(deq.begin() + idx);
